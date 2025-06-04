6297102acdde92b17f438c8cb804fd0de9a77b3c
Zytronium <dastelljes@outlook.com>
2025-06-03 21:22:56 -0500

    Did tasks 6-8 and attempted to fix app.py for flask_babel v4.X

--- localization/app.py
+++ localization/app.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python3
 """
-todo
+Main app script for localization example
 """
 
-from babel import Babel, _
-from flask_babel import ngettext
+from flask import Flask, request
+from flask_babel import Babel, _
 
-greeting = _("Hello, World!")
+print("Using Babel class from:", Babel.__module__)
 
-message = ngettext("You have one message", "You have %(num)d messages", num)
+app = Flask(__name__)
+babel = Babel(app)
+
+# Configure available languages
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_TRANSLATION_DIRECTORIES'] = './translations'
+
+class MyBabel(Babel):
+    def get_locale(self):
+        # Use request headers or a user-specific preference to determine locale
+        return request.accept_languages.best_match(['en', 'fr'])
+
+@app.route('/')
+def index():
+    # This will display the string in the appropriate language
+    return _("Hello, World!")
+
+if __name__ == "__main__":
+    app.run()
